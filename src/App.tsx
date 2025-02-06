fe2ae0503ae2a697e9f1c2191ec4c87869e87e2b
nowscott <nowscott@qq.com>
2025-02-06 11:29:46 +0800

    🦄 refactor: 删除空行

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ function App() {
   useEffect(() => {
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
     setIsDarkMode(mediaQuery.matches);
-
     const handler = (e: MediaQueryListEvent) => setIsDarkMode(e.matches);
     mediaQuery.addEventListener('change', handler);
     return () => mediaQuery.removeEventListener('change', handler);
